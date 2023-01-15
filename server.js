d12bce41672f8c0305557f189eb49fe087748428
Annie Huang <annie.hn@gmail.com>
2023-01-15 14:37:47 +1100

    Add graphql schema and test it worked.

--- server.js
+++ server.js
@@ -1,3 +1,27 @@
 const express = require('express');
+const expressGraphQL = require('express-graphql').graphqlHTTP;
+const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require('graphql');
 const app = express();
+
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'HelloWorld',
+    description: 'Root Query',
+    fields: () => ({
+      message: {
+        type: GraphQLString,
+        resolve: () => 'Hello World',
+      },
+    }),
+  }),
+  // mutation: RootMutationType,
+});
+
+app.use(
+  '/graphql',
+  expressGraphQL({
+    schema: schema,
+    graphiql: true,
+  })
+);
 app.listen(5000, () => console.log('Server Running'));
